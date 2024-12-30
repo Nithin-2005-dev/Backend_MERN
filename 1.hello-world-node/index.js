6f01a38301d1679dce461877181ca2051d7b6aa2
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2024-12-31 00:35:29 +0530

    hello world with node

---
+++ 1.hello-world-node/index.js
@@ -0,0 +1,8 @@
+
+console.log("hello node js");
+const arr=[1,2,3,4,5];
+console.log(arr);
+setTimeout(()=>{
+console.log("i'm in asyncronous function");
+},2000);
+console.log("this is the last line of syncronous code");
